b07ac4ca90f98945d20c80636ab2f26f49871838
Hutecker, Stephan <Stephan.Hutecker@crowehorwath.com>
2017-08-17 13:46:34 -0500

    Added shark names

--- croweDemo/src/data/in-memory-data.service.ts
+++ croweDemo/src/data/in-memory-data.service.ts
@@ -2,12 +2,12 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
     createDb() {
         const sharks = [
-            { id: 0, name: 'Shark0', rating: 9 },
-            { id: 1, name: 'Shark1', rating: 1 },
-            { id: 2, name: 'Shark2', rating: 8 },
-            { id: 3, name: 'Shark3', rating: 3 },
-            { id: 4, name: 'Shark4', rating: 6 },
-            { id: 5, name: 'Shark5', rating: 4 }
+            { id: 0, name: 'Bob', rating: 9 },
+            { id: 1, name: 'Bill', rating: 1 },
+            { id: 2, name: 'Mandy', rating: 8 },
+            { id: 3, name: 'Rebecca', rating: 3 },
+            { id: 4, name: 'Laura', rating: 6 },
+            { id: 5, name: 'Alex', rating: 4 }
         ];
         return { sharks };
     }
