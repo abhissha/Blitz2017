b1f9d13497788076a4b8792b70d6ca8798ffe05e
Hutecker, Stephan <Stephan.Hutecker@crowehorwath.com>
2017-08-17 10:22:31 -0500

    Added shark images

--- croweDemo/src/data/in-memory-data.service.ts
+++ croweDemo/src/data/in-memory-data.service.ts
@@ -7,9 +7,7 @@ export class InMemoryDataService implements InMemoryDbService {
             { id: 2, name: 'Shark2' },
             { id: 3, name: 'Shark3' },
             { id: 4, name: 'Shark4' },
-            { id: 5, name: 'Shark5' },
-            { id: 6, name: 'Shark6' },
-            { id: 7, name: 'Shark7' }
+            { id: 5, name: 'Shark5' }
         ];
         return { sharks };
     }
