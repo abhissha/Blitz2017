39b73b4d17e86095a41d21544a22f8abc8d5afca
Hutecker <HuteckerS@crowe-chizek.com>
2017-10-10 13:15:02 -0500

    Mulitple components done

--- croweDemo/src/app/app.module.ts
+++ croweDemo/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }        from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports: [
@@ -10,7 +11,8 @@ import { AppComponent }  from './app.component';
     FormsModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   bootstrap: [ AppComponent ]
 })
--- croweDemo/src/app/hero-detail.component.ts
+++ croweDemo/src/app/hero-detail.component.ts
@@ -1,37 +1,20 @@
-import 'rxjs/add/operator/switchMap';
-import { Component, OnInit }        from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Location }                 from '@angular/common';
+import { Component, Input } from '@angular/core';
 
-import { Hero }        from './hero';
-import { HeroService } from './hero.service';
-
+import { Hero } from './hero';
 @Component({
   selector: 'hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: [ './hero-detail.component.css' ]
+  template: `
+    <div *ngIf="hero">
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name"/>
+      </div>
+    </div>
+  `
 })
-export class HeroDetailComponent implements OnInit {
-  hero: Hero;
-
-  constructor(
-    private heroService: HeroService,
-    private route: ActivatedRoute,
-    private location: Location
-  ) {}
-
-  ngOnInit(): void {
-    this.route.paramMap
-      .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
-      .subscribe(hero => this.hero = hero);
-  }
-
-  save(): void {
-    this.heroService.update(this.hero)
-      .then(() => this.goBack());
-  }
-
-  goBack(): void {
-    this.location.back();
-  }
+export class HeroDetailComponent {
+  @Input() hero: Hero;
 }
+
