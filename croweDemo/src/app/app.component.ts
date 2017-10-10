040d73edf2e28a066249315c6185fa234debd0d8
Hutecker <HuteckerS@crowe-chizek.com>
2017-10-10 13:12:26 -0500

    Hero editor done

--- croweDemo/src/app/app.component.ts
+++ croweDemo/src/app/app.component.ts
@@ -1,17 +1,26 @@
-import { Component }          from '@angular/core';
+import { Component } from '@angular/core';
+
+export class Hero {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <nav>
-      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `,
-  styleUrls: ['./app.component.css']
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
