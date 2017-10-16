dde6d95525e61f29ff0d529b46c1a1b5a48217ab
abhi.sharma <abhi.sharma@crowehorwath.com>
2017-10-16 14:20:39 -0500

    update services cli

--- croweDemo/src/app/app.module.ts
+++ croweDemo/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
-import { AppComponent }        from './app.component';
+import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
---
+++ croweDemo/src/app/hero.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+
+  // See the "Take it slow" appendix
+  getHeroesSlowly(): Promise<Hero[]> {
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(this.getHeroes()), 2000);
+    });
+  }
+}
+
+
+/*
+Copyright 2017 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
